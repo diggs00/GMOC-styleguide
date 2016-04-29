6751322a0f98c9aacb88279aadae94719a18d761
David Baker <dbaker@k6b9.com>
2016-04-29 13:31:50 -0500

    tables

--- js/components/responsive-tables.js
+++ js/components/responsive-tables.js
@@ -17,13 +17,13 @@ $(document).ready(function() {
   };
    
 
-  // $(window).load(updateTables);
-  // $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  // $(window).on("resize", updateTables);
+  $(window).load(updateTables);
+  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
+  $(window).on("resize", updateTables);
 
-  $( "#radio-table" ).load(updateTables);
-  $( "#radio-table" ).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $( "#radio-table" ).on("resize", updateTables);
+  // $( "#radio-table" ).load(updateTables);
+  // $( "#radio-table").on("redraw",function(){switched=false;updateTables();}); // An event to listen for
+  // $( "#radio-table" ).on("resize", updateTables);
    
 	
 	function splitTable(original)
